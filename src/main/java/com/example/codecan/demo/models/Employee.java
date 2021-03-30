27af95f11afde07df3795fec449f5b72052b5290
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-30 11:47:06 +0100

    created Employee class

---
+++ src/main/java/com/example/codecan/demo/models/Employee.java
@@ -0,0 +1,53 @@
+package com.example.codecan.demo.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employeeId")
+    private Long employeeId;
+    @Column(name = "first_name")
+    private String first_name;
+    @Column(name = "last_name")
+    private String last_name;
+//    private Department department;
+//    private List<Project> projects;
+
+
+    public Employee(String first_name, String last_name) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+    public Employee(){
+
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+}
