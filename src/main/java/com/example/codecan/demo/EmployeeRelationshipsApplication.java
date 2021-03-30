ecedc3dff25016b9a6f48edcb32526d321cab8d6
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-30 11:28:13 +0100

    first commit

---
+++ src/main/java/com/example/codecan/demo/EmployeeRelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.example.codecan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRelationshipsApplication.class, args);
+	}
+
+}
