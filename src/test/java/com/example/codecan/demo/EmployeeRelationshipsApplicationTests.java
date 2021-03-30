8b1e7313f1916a41bbebb98f4b15a24bb7e4b0bb
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-30 11:55:47 +0100

    sucessfully added employee to db

--- src/test/java/com/example/codecan/demo/EmployeeRelationshipsApplicationTests.java
+++ src/test/java/com/example/codecan/demo/EmployeeRelationshipsApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.codecan.demo;
 
+import com.example.codecan.demo.models.Employee;
+import com.example.codecan.demo.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class EmployeeRelationshipsApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void createEmployee(){
+		Employee employee = new Employee("Bob", "Bobberson");
+		employeeRepository.save(employee);
+
 	}
 
 }
